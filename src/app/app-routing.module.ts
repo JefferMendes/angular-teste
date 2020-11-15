5aa4fa8cec8ced137ad7f726561ad1fc44ad95cc
Jefferson Mendes <emanuel.vasconcelos@funceme.br>
2020-11-15 20:30:45 -0300

    add authentication route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
+import { LoginComponent } from './account/login/login.component';
+import { AuthGuard } from './service/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {path: '', component: HomeComponent}
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      {path: '', redirectTo: 'login', pathMatch: 'full'},
+      {path: 'login', component: LoginComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
